1c6cd1c56c858b64f92cb5304236b7b259136ae4
rexbr_3615 <83562258+rexbr3615@users.noreply.github.com>
2023-05-09 20:03:17 -0300

    anyway

--- src/main/java/net/rexbr/remrem/Remrem.java
+++ src/main/java/net/rexbr/remrem/Remrem.java
@@ -7,11 +7,12 @@ public final class Remrem extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("RemRem Has Sucefully Started");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("RemRem Has Sucefully Terminated");
     }
 }
--- src/main/java/net/rexbr/remrem/items/PlayerHandler.java
+++ src/main/java/net/rexbr/remrem/items/PlayerHandler.java
@@ -1,4 +1,14 @@
 package net.rexbr.remrem.items;
 
-public class PlayerHandler {
+import net.rexbr.remrem.Remrem;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+
+public class PlayerHandler implements Listener {
+    public PlayerHandler(Remrem plugin) {
+        Bukkit.getPluginManager().registerEvents(this, plugin);
+    }
+
+
+
 }
