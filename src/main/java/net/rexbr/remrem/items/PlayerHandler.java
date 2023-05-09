08fa14951cf3f9124ea4b7a65111e38469b02036
rexbr_3615 <83562258+rexbr3615@users.noreply.github.com>
2023-05-09 20:06:39 -0300

    anyway 2

--- src/main/java/net/rexbr/remrem/items/PlayerHandler.java
+++ src/main/java/net/rexbr/remrem/items/PlayerHandler.java
@@ -2,13 +2,19 @@ package net.rexbr.remrem.items;
 
 import net.rexbr.remrem.Remrem;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerHandler implements Listener {
     public PlayerHandler(Remrem plugin) {
         Bukkit.getPluginManager().registerEvents(this, plugin);
     }
 
-
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+       Player player = event.getPlayer();
+    }
 
 }
