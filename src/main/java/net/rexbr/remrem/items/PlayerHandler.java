dc2bbe267196148be0ee4ba8892a54fd1e04f081
rexbr_3615 <83562258+rexbr3615@users.noreply.github.com>
2023-05-09 21:20:58 -0300

    anyway 4

--- src/main/java/net/rexbr/remrem/items/PlayerHandler.java
+++ src/main/java/net/rexbr/remrem/items/PlayerHandler.java
@@ -2,6 +2,7 @@ package net.rexbr.remrem.items;
 
 import net.rexbr.remrem.Remrem;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
@@ -12,6 +13,9 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerHandler implements Listener {
     public PlayerHandler(Remrem plugin) {
         Bukkit.getPluginManager().registerEvents(this, plugin);
@@ -21,14 +25,13 @@ public class PlayerHandler implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event){
        Player player = event.getPlayer();
 
-        ItemStack item = new ItemStack(Material.STICK, 10);
-        Inventory inv = player.getInventory();
+        ItemStack hell_stick = new ItemStack(Material.STICK, 1);
+        ItemMeta meta = hell_stick.getItemMeta();
+        List<String> lore = new ArrayList<String>();
+        lore.add(ChatColor.GRAY + "An Strange Stick");
+        meta.setLore(lore);
+        hell_stick.setItemMeta(meta);
 
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName("Hell Stick");
-        item.setItemMeta(meta);
-
-        inv.addItem(item);
     }
 
 }
